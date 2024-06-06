2a04ec0ce0c1e899f859ec012bf5ceb4f904f724
Richard Armijos <richardmijo@hotmail.com>
2024-06-05 19:31:41 -0500

    Proyecto inicial

---
+++ src/main/java/ec/edu/uide/estructura/semana_6/Semana_6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ec.edu.uide.estructura.semana_6;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class Semana_6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
